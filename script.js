d61d94bf636a962bcd0189f73a75d58cd1e60de0
Beck Davies <beck.davies@therealbuzzgroup.com>
2017-08-04 23:44:30 +0100

    splitting out the css and js into their own separate files

---
+++ script.js
@@ -0,0 +1,7 @@
+   
+var rows = document.getElementsByClassName('flex-container--row');
+//rows.classList.add("js-row");
+rows.className += " otherclass";
+
+var cols = document.getElementsByClassName('flex-container--col');
+//cols.classList.add("js-col");
